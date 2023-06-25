35d080a9bb5b41c1072ebc00fde40083d19f9fe3
jannispaul <jannis@pixel-pink.de>
2023-06-25 19:28:31 +0200

    added animations and fixes

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// @ts-check
+import { createClock } from "./createClock";
+import { animateHome } from "./animateHome";
+// console.log("test");
+createClock();
+console.log("hi");
+animateHome();
+window.addEventListener("DOMContentLoaded", (event) => {});
