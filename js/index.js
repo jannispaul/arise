c83c09477344cdec433f0d040e7ce98131ed3434
jannispaul <jannis@pixel-pink.de>
2024-06-28 07:51:47 +0200

    removed clock and project hover

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
 // @ts-check
-import { createClock } from "./createClock";
 import { animateElements } from "./animateElements";
 import { pageTransitions } from "./pageTransitions";
 import { prettyLinks } from "./prettyLinks";
 import { processHover } from "./processHover";
-import { projectHover } from "./projectHover";
 import { magneticHover } from "./magneticHover";
 import { smartVideo } from "./smartVideo";
 
 function init() {
   smartVideo();
-  createClock();
   animateElements();
   pageTransitions();
   prettyLinks();
   processHover();
-  projectHover();
   magneticHover();
 }
 
